9f87c72b73ad6b7738e5acbebb4ce2032b48143c
mh032611 <mh032611@gmail.com>
2023-07-27 01:15:03 -0500

    Update data_scraper_script.py

--- data_scraper_script.py
+++ data_scraper_script.py
@@ -3,6 +3,6 @@ import requests
 
 page_to_scrape_houston = requests.get("https://www.sports-reference.com/cfb/schools/houston/2022.html#all_team")
 soup = BeautifulSoup(page_to_scrape_houston.text, "html.parser")
- #https://www.sports-reference.com/cfb/schools/houston/2022.html#all_team
-
+#https://www.sports-reference.com/cfb/schools/houston/2022.html#all_team
+# https://www.espn.com/college-football/team/stats/_/id/248/season/2022
 QBs = soup.find_all                                     
